2579c785af45a2a81e102f1e7ec745d1fdc45bd1
Nikolai Ruhe <nikoruhe54@gmail.com>
2018-04-25 19:14:42 -0400

    test 1

--- SeamCarve/SeamCarve/CarveImage.cpp
+++ SeamCarve/SeamCarve/CarveImage.cpp
@@ -3,3 +3,31 @@
 #include <fstream>
 using namespace std;
 
+int** uploadData(string FileName) {
+	int xLength, yLength, maxVal;
+	string word = "";
+	char letter;
+	bool p2Check = false, dimensionCheck = false, maxCheck = false;
+	ifstream inFile(FileName);
+	while (inFile) {
+		inFile >> letter;
+		if (letter == '\n') {
+			if (!p2Check) {
+				if (word == "P2") {
+					p2Check == true;
+				}
+			}
+			else if (!dimensionCheck) {
+
+			}
+		}
+		else if (letter == '#') {
+
+		}
+		else if (letter == ' ') {
+
+		}
+		word += letter;
+	}
+
+}

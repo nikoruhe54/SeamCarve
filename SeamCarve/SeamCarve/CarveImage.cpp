e99f7f6ba1694da82c0b893418034050fee29455
Nikolai Ruhe <nikoruhe54@gmail.com>
2018-04-25 20:00:32 -0400

    try to build pixel str

--- SeamCarve/SeamCarve/CarveImage.cpp
+++ SeamCarve/SeamCarve/CarveImage.cpp
@@ -1,33 +1,75 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
-int** uploadData(string FileName) {
+string uploadData(string FileName) {
 	int xLength, yLength, maxVal;
-	string word = "";
+	string word = "", dimension = "", maxSize = "", pixelStr = "";
 	char letter;
-	bool p2Check = false, dimensionCheck = false, maxCheck = false;
+	bool p2Check = false, dimensionCheck = false, maxCheck = false, comment = false;
 	ifstream inFile(FileName);
 	while (inFile) {
+		comment = false;
 		inFile >> letter;
 		if (letter == '\n') {
 			if (!p2Check) {
 				if (word == "P2") {
 					p2Check == true;
+					word = "";
+					cout << "got P2" << endl;
 				}
 			}
 			else if (!dimensionCheck) {
-
+				for (int i = 0; i < word.length(); i++) {
+					if (word[i] != ' ' && word[i] != '\n') {
+						dimension += word[i];
+					}
+					else if (word[i] == ' ') {
+						xLength = atoi(dimension.c_str());
+						dimension = "";
+						cout << "got x" << endl;
+					}
+					else if (word[i] == '\n') {
+						yLength = atoi(dimension.c_str());
+						dimension = "";
+						dimensionCheck = true;
+						cout << "got y" << endl;
+					}
+				}
+			}
+			else if (!maxCheck) {
+				if (letter != ' ' || letter != '\n') {
+					maxSize += letter;
+				}
+				else {
+					maxVal = atoi(maxSize.c_str());
+					maxCheck = true;
+					cout << "got maxSize" << endl;
+				}
+			}
+			else {
+				pixelStr += word;
+				word = "";
 			}
 		}
 		else if (letter == '#') {
-
+			while (letter != '\n') {
+				inFile >> letter;
+			}
+			comment = true;
 		}
-		else if (letter == ' ') {
 
+		if (comment == false) {
+			word += letter;
 		}
-		word += letter;
 	}
-
+	return pixelStr;
 }
+
+void main() {
+	string pixels = uploadData("test.pgm");
+	cout << pixels;
+}
+

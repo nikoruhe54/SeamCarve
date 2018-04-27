5f743a4ddacb1b380db84247f03027e8290e357c
Nikolai Ruhe <nikoruhe54@gmail.com>
2018-04-26 21:28:37 -0400

    work damnit

--- SeamCarve/SeamCarve/CarveImage.cpp
+++ SeamCarve/SeamCarve/CarveImage.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-string uploadData(string FileName, int &x, int &y, int &max) {
+int** uploadData(string FileName) {
 	int xLength = 0, yLength = 0, maxVal = 0;
 	string word = "", dimension = "", maxSize = "", pixelStr = "";
 	string xNum = "", yNum = "";
@@ -42,120 +42,20 @@ string uploadData(string FileName, int &x, int &y, int &max) {
 	xLength = atoi(xNum.c_str());
 	yLength = atoi(yNum.c_str());
 
-	while (inFile) {
-		getline(inFile, word);
-		if (word[0] != '#') {
-			pixelStr += word;
+	int** pixelMatrix = new int*[yLength];
+	for (int j = 0; j < yLength; j++) {
+		pixelMatrix[j] = new int[xLength];
+	}
+	for (int y = 0; y < yLength; y++) {
+		for (int x = 0; x < xLength; x++) {
+			inFile >> pixelMatrix[y][x];
 		}
 	}
-	return pixelStr;
-	/*
-	while (inFile.get(letter)) {
-		comment = false;
-		if (letter == '\n') {
-			if (!p2Check) {
-				if (word == "P2") {
-					p2Check = true;
-					word = "";
-				}
-			}
-			else if (!dimensionCheck) {
-				for (int i = 0; i < word.length(); i++) {
-					if (word[i] != ' ' && word[i] != '\n') {
-						dimension += word[i];
-					}
-					else if (word[i] == ' ') {
-						if (xLength == 0) {
-							xLength = atoi(dimension.c_str());
-							dimension = "";
-						}
-					}
-					 if (i == word.length()-1) {
-						yLength = atoi(dimension.c_str());
-						dimension = "";
-						dimensionCheck = true;
-						word = "";
-					}
-				}
-			}
-			else if (!maxCheck) {
-				maxVal = atoi(word.c_str());
-				maxCheck = true;
-				word = "";
-			}
-			else {
-				pixelStr += word;
-				pixelStr += "\n";
-				word = "";
-			}
-		}
-		else if (letter == '#') {
-			while (letter != '\n') {
-				inFile.get(letter);
-			}
-			comment = true;
-		}
-
-		if (comment == false && letter != '\n') {
-			word += letter;
-		}
-	}
-	x = xLength;
-	y = yLength;
-	max = maxVal;
-	return pixelStr;
-	*/
+	return pixelMatrix;
 }
 
-/*
-void buildPixelMatrix(int** &pixelMatrix, string pixels, int xLen, int yLen) {
-	int xCount = 0, yCount = 0;
-	string tempNum = "";
-	for (int i = 0; i < pixels.length(); i++) {
-		if (xCount < xLen) {
-			if (pixels[i] != ' ' && pixels[i] != '\n' && pixels[i] != '\t') {
-				tempNum += pixels[i];
-			}
-			else {
-				pixelMatrix[xCount][yCount] = atoi(tempNum.c_str());
-				tempNum = "";
-				xCount++;
-			}
-		}
-		else {
-			if (pixels[i] != ' ' && pixels[i] != '\n' && pixels[i] != '\t') {
-				tempNum += pixels[i];
-			} 
-			else {
-				yCount++;
-				xCount = 0;
-				pixelMatrix[xCount][yCount] = atoi(tempNum.c_str());
-				tempNum = "";
-				xCount++;
-			}
-		}
-	}
-}
-*/
 int main() {
-	int xLen = 0, yLen = 0, maxLen = 0;
-	string pixels = uploadData("bug.pgm", xLen, yLen, maxLen);
-	int** pixelMatrix = new int*[yLen];
-	for (int j = 0; j < yLen; j++) {
-		pixelMatrix[j] = new int[xLen];
-	}
-	//buildPixelMatrix(pixelMatrix, pixels, xLen, yLen);
-	/*
-	for (int i = 0; i < yLen; i++) {
-		for (int a = 0; a < xLen; a++) {
-			cout << pixelMatrix[a][i] << " ";
-		}
-		cout << endl;
-	}
-	*/
-	while (1) {
-
-	}
+	int** pixelMatrix = uploadData("test.pgm");
 	return 0;
 }
 
